403b00ac6cea3ff8c931878b3012c419db39bae5
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-15 00:14:31 +0700

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Contact from './pages/contact/Contact';
+import Home from './pages/home/Home';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
