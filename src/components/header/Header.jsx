70824fb4e96ed5a144d7200ee591521d9c67b72c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-15 00:40:00 +0700

    header component-logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
+const logo = (
+  <div className={styles.logo}>
+    <Link to="/">
+      <h2>
+        e<span>Shop</span>.
+      </h2>
+    </Link>
+  </div>
+);
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className={styles.header}>{logo}</div>
+    </header>
+  );
 };
 
 export default Header;
