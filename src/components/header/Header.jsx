22afa3d31386a5899c158d6114730d0917cab9b0
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-12-28 16:29:36 +0700

    header-nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const logo = (
   <div className={styles.logo}>
@@ -12,10 +13,41 @@ const logo = (
   </div>
 );
 
+const cart = (
+  <span className={styles.cart}>
+    <Link to="/cart">
+      Cart
+      <FaShoppingCart size={20} />
+      <p>0</p>
+    </Link>
+  </span>
+);
+
 const Header = () => {
   return (
     <header>
-      <div className={styles.header}>{logo}</div>
+      <div className={styles.header}>
+        {logo}
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+          <div className={styles['header-right']}>
+            <span className={styles.links}>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/order-history">My Orders</Link>
+            </span>
+            {cart}
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
