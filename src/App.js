11dda4d31138f2c2d27c0304c15f7bb7694b9bf9
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-12-28 13:45:18 +0700

    warning and error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import './App';
 
 function App() {
-  return <div className="App">Hello</div>;
+  const [first, setFirst] = useState('Master');
+
+  return (
+    <div className="App">
+      <h1>Hello {first}</h1>
+    </div>
+  );
 }
 
 export default App;
